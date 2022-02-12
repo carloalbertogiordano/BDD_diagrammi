25fcea6d69042db6c64f3a234ebf3ff3f2a05f6e
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2022-02-12 17:18:21 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -13,25 +13,21 @@ From Clienti C, Musei M, Visita V
     AND V.codiceMuseo = M.codice
     AND M.nome = "Museo bellissimo";
 
-#3) Mostrare la somma delle quotazioni di tutti i dipinti
+#3) Mostrare la somma delle quotazioni di tutti i dipinti (sommaQuotazioni)
 Select SUM(quotazione) from Dipinto;
 
-#4) Mostrare la somma delle quotazioni dei quadri presenti ad ogni mostra
+#4) Mostrare la somma delle quotazioni dei quadri presenti ad ogni mostra (codiceMuseo, dataOraInizio, dataOraFine, sommaQuotazioni)
 Select M.codiceMuseo, M.dataOraInizio, M.dataOraFine, SUM(D.quotazione)
 From MostraDipinto M, Dipinto D
 	Where M.nomeDipinto = D.nome
     AND M.numeroDipinto = D.numero
     Group by M.codiceMuseo, M.dataOraInizio, M.dataOraFine;
     
-#5) Mostrare la somma delle quotazioni delle sculture appartenenti al genere classico con una quotazione superiore a 10.000 euro
-select SUM(quotazione)
-from Scultura
-where genere = "classico"
-AND quotazione >= 10000
+#5) Mostrare gli artisti che hanno creato più di un dipinto (nome, ddnascita, #dipinti)
 
-#6) Elencare il genere per il quale la somma delle quotazioni delle sculture che gli appartengono è la più alta
+#6) Elencare il genere per il quale la somma delle quotazioni delle sculture che gli appartengono è la più alta (genere, #sommaQuotazioni)
 
-#7) Elencare tutti i dipinti che sono stato esposti al "museo di arte" ma non al "museo delle opere"
+#7) Elencare tutti i dipinti che sono stato esposti al "museo di arte" ma non al "museo delle opere" (Nome, Numero, dataOraInizio, dataOraFine)
 
 #8) Selezionare i clienti che hanno visitato tutti i musei
 
