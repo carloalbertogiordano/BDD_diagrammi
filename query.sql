c8174dd5bbeb6f9156362c8622b9b2a083bc8854
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2022-02-13 19:05:05 +0100

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,73 +1,67 @@
-use DBMuseo;
+USE DBMuseo;
 
 #1) Elencare, ordinatamente in base alla data di creazione, tutti i dipinti che sono stati creati da Dalì e tutti i dipindi creati da Botticelli. (Nome, Numero, Data di creazione)
-Select D.nome, D.numero, D.dataCreazione
-From Dipinto D, Artista A 
-	Where D.ID_Artista = A.ID
+SELECT D.nome, D.numero, D.dataCreazione
+FROM Dipinto D, Artista A 
+	WHERE  D.ID_Artista = A.ID
     AND (A.nome = "Dalì" OR A.nome = "Botticelli");
     
 #2) Elencare tutti i clienti che hanno visitato il "museo bellissimo" (Nome, Cognome)
-Select C.nome, C.cognome
-From Clienti C, Musei M, Visita V
-	Where C.ID = V.ID_Cliente
+SELECT C.nome, C.cognome
+FROM Clienti C, Musei M, Visita V
+	WHERE C.ID = V.ID_Cliente
     AND V.codiceMuseo = M.codice
     AND M.nome = "Museo bellissimo";
 
 #3) Mostrare la somma delle quotazioni di tutti i dipinti (sommaQuotazioni)
-Select SUM(quotazione) from Dipinto;
+SELECT SUM(quotazione) FROM Dipinto;
 
 #4) Mostrare la somma delle quotazioni dei quadri presenti ad ogni mostra (codiceMuseo, dataOraInizio, dataOraFine, sommaQuotazioni)
-Select M.codiceMuseo, M.dataOraInizio, M.dataOraFine, SUM(D.quotazione)
-From MostraDipinto M, Dipinto D
-	Where M.nomeDipinto = D.nome
+SELECT M.codiceMuseo, M.dataOraInizio, M.dataOraFine, SUM(D.quotazione)
+FROM MostraDipinto M, Dipinto D
+	WHERE M.nomeDipinto = D.nome
     AND M.numeroDipinto = D.numero
-    Group by M.codiceMuseo, M.dataOraInizio, M.dataOraFine;
+    GROUP BY M.codiceMuseo, M.dataOraInizio, M.dataOraFine;
     
 #5) Mostrare gli artisti che hanno creato più di un dipinto (nome, ddnascita, #dipinti)
-Select A.nome, A.ddnascita, count(*) as conta
-from Artista A, Dipinto D
-Where D.ID_Artista = A.ID
-group by(A.ID) having conta > 2;
+SELECT A.nome, A.ddnascita, count(*) as conta
+FROM Artista A, Dipinto D
+	WHERE D.ID_Artista = A.ID
+	GROUP BY A.ID HAVING conta > 2;
 
 #6) Elencare il genere per il quale la somma delle quotazioni delle sculture che gli appartengono è la più alta (genere, #sommaQuotazioni)
-Drop view if exists SommaQuotazioniSculture;
-create view SommaQuotazioniSculture as (Select S.genere, SUM(S.quotazione) as somma
-										from Scultura S
-                                        group by S.genere
+DROP VIEW IF EXISTS SommaQuotazioniSculture;
+CREATE VIEW SommaQuotazioniSculture AS (SELECT S.genere, SUM(S.quotazione) AS somma
+										FROM Scultura S
+                                        GROUP BY S.genere
 );
-Select * 
-from SommaQuotazioniSculture SQS
-where SQS.somma = (Select MAX(somma) from SommaQuotazioniSculture);
+
+SELECT * 
+FROM SommaQuotazioniSculture SQS
+	WHERE SQS.somma = (SELECT MAX(somma) FROM SommaQuotazioniSculture);
 
 #7) Elencare tutti i dipinti che sono stato esposti al "museo di arte" ma non al "museo delle opere" (Nome, Numero, dataOraInizio, dataOraFine)
-Select Mo.nomeDipinto, Mo.numeroDipinto, Mo.dataOraInizio, Mo.dataOraFine
-from MostraDipinto Mo, Musei Mu
-where Mo.codiceMuseo = Mu.codice
-AND Mu.nome = "Museo di arte"
-AND (Mo.nomeDipinto, Mo.numeroDipinto) NOT IN (Select Mo.nomeDipinto, Mo.numeroDipinto
-						from MostraDipinto Mo, Musei Mu
-						where Mo.codiceMuseo = Mu.codice
-						AND Mu.nome = "Museo delle opere"
-                                                );
+SELECT Mo.nomeDipinto, Mo.numeroDipinto, Mo.dataOraInizio, Mo.dataOraFine
+FROM MostraDipinto Mo, Musei Mu
+	WHERE  Mo.codiceMuseo = Mu.codice
+	AND Mu.nome = "Museo di arte"
+	AND (Mo.nomeDipinto, Mo.numeroDipinto) NOT IN (
+		SELECT Mo.nomeDipinto, Mo.numeroDipinto
+		FROM MostraDipinto Mo, Musei Mu
+			WHERE Mo.codiceMuseo = Mu.codice
+			AND Mu.nome = "Museo delle opere"
+            );
 
 #8) Selezionare i clienti che hanno visitato tutti i musei (idCliente, nome, cognome)
-Select C.ID, C.nome, C.cognome
-from Clienti C
-where NOT EXISTS (Select * 
-			from Musei M 
-			where NOT EXISTS (Select * 
-			from Visita V
-                        where V.ID_Cliente = C.ID
-			AND V.codiceMuseo = M.codice
-                 	)
+SELECT C.ID, C.nome, C.cognome
+FROM Clienti C
+WHERE NOT EXISTS (
+	SELECT * 
+	FROM Musei M 
+		WHERE NOT EXISTS (
+			SELECT * 
+			FROM Visita V
+				WHERE V.ID_Cliente = C.ID
+				AND V.codiceMuseo = M.codice
+				)
 		);
-
-
-
-
-
-
-
-
-
-
