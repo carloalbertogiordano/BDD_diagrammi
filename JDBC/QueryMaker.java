0651f92c85901cd07a856718f933c4597ec46fb4
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2022-02-14 11:03:05 +0100

    Create QueryMaker.java

---
+++ JDBC/QueryMaker.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class QueryMaker {
+
+    private Connection con;
+
+    public QueryMaker (String url, String user, String pass) {
+        String JdbcURL = url;
+        String Username = user;
+        String password = pass;
+        try {
+            con=DriverManager.getConnection(JdbcURL, Username, password);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Connection is successful!!!!!!");
+    }
+
+    public ArrayList makeQuery (String q) throws SQLException {
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(q);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        ArrayList<ArrayList> rQuery = new ArrayList<ArrayList>();        
+   
+        while(rs.next()){
+            ArrayList<String> tupla = new ArrayList<String>();
+            for(int i = 1; i <= columnsNumber; i++){
+                tupla.add(rs.getString(i));
+            }
+            rQuery.add(tupla);
+        }
+        return rQuery;
+    }
+}
