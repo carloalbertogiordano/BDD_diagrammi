faed6f0621fa936900950291d18473c11a1cca5d
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2022-02-15 17:19:20 +0100

    Update Tools.java

--- JDBC/QueryMaker/src/tools/Tools.java
+++ JDBC/QueryMaker/src/tools/Tools.java
@@ -4,22 +4,37 @@ import java.util.GregorianCalendar;
 
 public class Tools {
     static public boolean checkDate(String date1, String date2) {
-    	String[] s1 = date1.split("-");
-    	String[] s2 = date2.split("-");
+    	String[] dataOra1 = date1.split(" ");
+    	String[] dataOra2 = date2.split(" ");
     	
-    	int month1 = Integer.parseInt(s1[1]);
-    	int month2 = Integer.parseInt(s2[1]);
+    	String[] data1 = dataOra1[0].split("-");
+    	String[] data2 = dataOra2[0].split("-");
     	
-    	if(s1[0].matches("\\d\\d\\d\\d") && s1[1].matches("\\d\\d") && s1[0].matches("\\d\\d") && month1<13 && month1>0) {
+    	String[] ora1 = dataOra1[1].split(":");
+    	String[] ora2 = dataOra2[1].split(":");
+    	
+    	int month1 = Integer.parseInt(data1[1]);
+    	int month2 = Integer.parseInt(data2[1]);
+    	
+    	int hour1 = Integer.parseInt(ora1[0]);
+    	int hour2 = Integer.parseInt(ora2[0]);
+    	    	
+    	int minute1 = Integer.parseInt(ora1[1]);
+    	int minute2 = Integer.parseInt(ora2[1]);
+    	
+    	int second1 = Integer.parseInt(ora1[2]);
+    	int second2 = Integer.parseInt(ora2[2]);
+    	
+    	if(data1[0].matches("\\d\\d\\d\\d") && data1[1].matches("\\d\\d") && data1[0].matches("\\d\\d") && month1<13 && month1>0) {
     		return false;
     	}
     	
-    	if(s2[0].matches("\\d\\d\\d\\d") && s2[1].matches("\\d\\d") && s2[0].matches("\\d\\d") && month2<13 && month2>0) {
+    	if(data2[0].matches("\\d\\d\\d\\d") && data2[1].matches("\\d\\d") && data2[0].matches("\\d\\d") && month2<13 && month2>0) {
     		return false;
     	}
     	
-    	GregorianCalendar d = new GregorianCalendar(Integer.parseInt(s1[0]), month1, Integer.parseInt(s1[2]));
-    	GregorianCalendar d2 = new GregorianCalendar(Integer.parseInt(s2[0]), month2, Integer.parseInt(s2[2]));
+    	GregorianCalendar d = new GregorianCalendar(Integer.parseInt(data1[0]), month1-1, Integer.parseInt(data1[2]), hour1, minute1, second1);
+    	GregorianCalendar d2 = new GregorianCalendar(Integer.parseInt(data2[0]), month2-1, Integer.parseInt(data2[2]), hour2, minute2, second2);
     	
     	if(d2.before(d))
     		return false;
